2b337d920ab484359ccdf2b7a1c17b209e336a2b
Muzuvukile Nqwiliso <mzu.nqwiliso@gmail.com>
2026-05-15 00:12:55 +0200

    Update button link to new Google Drive URL

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -80,7 +80,7 @@ export default function Header({ links, buttonText }) {
         text={buttonText}
         onClick={() =>
           window.open(
-            "https://drive.google.com/file/d/1HHFAOQ6ts_rNjeNNPUWGFa8OX8RYdK7s/view?usp=drive_link",
+            "https://drive.google.com/file/d/11Gyd7TCJSTqhCkUo55g3WIBBAcgueWXz/view?usp=sharing",
             "_blank",
           )
         }
