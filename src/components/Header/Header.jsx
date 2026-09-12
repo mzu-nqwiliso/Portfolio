71da5fd86fda0d83cd1adbe64ec51879b6652298
Muzuvukile Nqwiliso <mzu.nqwiliso@gmail.com>
2026-09-12 09:17:14 +0200

    Update link in Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -80,7 +80,7 @@ export default function Header({ links, buttonText }) {
         text={buttonText}
         onClick={() =>
           window.open(
-            "https://drive.google.com/file/d/11A1wLn3uesRD-qKHj8rmyvKBTWgi5zOb/view?usp=sharing",
+            "https://drive.google.com/file/d/1Gwzwe1dwFvB6KvEmoah8xyR9rkMpzV1f/view?usp=sharing",
             "_blank",
           )
         }
